86ba02684ce11a8b34b5a5f3b43ddd0b2cd3099b
Mohammed Ali <158100369+mohamedeltahan1470@users.noreply.github.com>
2025-03-19 14:52:55 +0200

    Update ScrollButton.jsx
    add scroll

--- src/component/ScrollButton.jsx
+++ src/component/ScrollButton.jsx
@@ -1,42 +1,77 @@
-// import React  from 'react';
+// // import React  from 'react';
+// import "./ScrollButton.css";
+
+// const ScrollButton = () => {
+//     const scrollStep = 50; 
+//     const scrollDuration = 15;
+//     const scrollToTop = () => {
+//         const scrollInterval = setInterval(() => {
+//             const currentScroll = window.scrollY;
+//             if (currentScroll > 0) {
+//                 window.scrollTo(0, currentScroll - scrollStep);
+//             } else {
+//                 clearInterval(scrollInterval); 
+//             }
+//         }, scrollDuration);
+//     };
+//     const scrollToBottom = () => {
+//         const scrollInterval = setInterval(() => {
+//             const currentScroll = window.scrollY;
+//             const windowHeight = window.innerHeight;
+//             const documentHeight = document.documentElement.scrollHeight;
+
+//             if (currentScroll < documentHeight - windowHeight) {
+//                 window.scrollTo(0, currentScroll + scrollStep);
+//             } else {
+//                 clearInterval(scrollInterval); 
+//             }
+//         }, scrollDuration);
+//     };
+//     return (
+//         <div className="scroll-button-container">
+//             <button className="scroll-button" onClick={scrollToTop}>
+//                 ↑ 
+//             </button>
+//             <button className="scroll-button" onClick={scrollToBottom}>
+//                 ↓ 
+//             </button>
+//         </div>
+//     );
+// };
+
+// export default ScrollButton;
+
 import "./ScrollButton.css";
 
 const ScrollButton = () => {
-    const scrollStep = 50; 
-    const scrollDuration = 15;
-    const scrollToTop = () => {
-        const scrollInterval = setInterval(() => {
+    const smoothScroll = (direction) => {
+        const scrollStep = 50;
+        const targetScroll = direction === "up" ? 0 : document.documentElement.scrollHeight;
+        
+        const scrollAnimation = () => {
             const currentScroll = window.scrollY;
-            if (currentScroll > 0) {
-                window.scrollTo(0, currentScroll - scrollStep);
-            } else {
-                clearInterval(scrollInterval); 
+            if (
+                (direction === "up" && currentScroll > 0) ||
+                (direction === "down" && currentScroll < document.documentElement.scrollHeight - window.innerHeight)
+            ) {
+                window.scrollBy(0, direction === "up" ? -scrollStep : scrollStep);
+                requestAnimationFrame(scrollAnimation);
             }
-        }, scrollDuration);
+        };
+        requestAnimationFrame(scrollAnimation);
     };
-    const scrollToBottom = () => {
-        const scrollInterval = setInterval(() => {
-            const currentScroll = window.scrollY;
-            const windowHeight = window.innerHeight;
-            const documentHeight = document.documentElement.scrollHeight;
 
-            if (currentScroll < documentHeight - windowHeight) {
-                window.scrollTo(0, currentScroll + scrollStep);
-            } else {
-                clearInterval(scrollInterval); 
-            }
-        }, scrollDuration);
-    };
     return (
         <div className="scroll-button-container">
-            <button className="scroll-button" onClick={scrollToTop}>
-                ↑ 
+            <button className="scroll-button" onClick={() => smoothScroll("up")}>
+                ↑
             </button>
-            <button className="scroll-button" onClick={scrollToBottom}>
-                ↓ 
+            <button className="scroll-button" onClick={() => smoothScroll("down")}>
+                ↓
             </button>
         </div>
     );
 };
 
 export default ScrollButton;
+
